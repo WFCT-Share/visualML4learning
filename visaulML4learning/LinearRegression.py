724e540244fa9e39dd85c047734f44b519a5764f
Zhihao Yu <yuzhihao82@gmail.com>
2024-05-27 17:44:41 +0800

    upgrade

---
+++ visaulML4learning/LinearRegression.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+"""设置字符集防止乱码问题"""
+plt.rcParams['font.sans-serif'] = [u'simHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+"""使用动态规划分步回归实现回归过程可视化  """
